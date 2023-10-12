ce9b9de5eb12e8fa0b0d27f0d5f84b8f0675cfc9
Adam Vessey <adam@discoverygarden.ca>
2023-10-12 16:40:16 -0300

    Add in some things added by `symfony/flex` that were missed.

---
+++ config/bundles.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Islandora\Crayfish\Commons\CrayfishCommonsBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+];
