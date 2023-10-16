5f43e5fca0e4a04fe3634bcd4cf45168bfa915fa
Adam Vessey <adam@discoverygarden.ca>
2023-10-16 17:11:53 -0300

    Poke misc.

--- config/bundles.php
+++ config/bundles.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    Islandora\Crayfish\Commons\CrayfishCommonsBundle::class => ['dev' => true, 'test' => true],
+    Islandora\Crayfish\Commons\CrayfishCommonsBundle::class => ['all' => true],
     Islandora\Crayfish\Commons\Syn\CrayfishCommonsSynBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
